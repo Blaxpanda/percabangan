d5ea13fb23aae9a1d6222d7c194703046797d171
Blaxpanda <115604090+Blaxpanda@users.noreply.github.com>
2022-10-12 10:19:40 +0700

    Add files via upload

---
+++ coba percabangan.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"masukkan nilai : ";
+    cin>>a;
+    if(a>=0 && a<=100){
+        cout<<"YA";
+    }
+    else{
+        cout<<"TIDAK";
+    }
+}
+
